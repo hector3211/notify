89e583373a5a3b80e00e539410f1add4e4fa1797
hector3211e <hectororopesa5@gmail.com>
2024-10-17 18:22:44 -0400

    added: sending emails

--- pkg/handlers/put_admin_editjob.go
+++ pkg/handlers/put_admin_editjob.go
@@ -43,19 +43,20 @@ func (h *PutAdminJobEditHandler) ServeHTTP(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	// TODO:
+	// TODO: get user email
+	var userEmail string = ""
 
 	// Send email notification
-	// if models.JobStatus(newStatus) == models.JOBDONE {
-	// 	msg := fmt.Sprintf("Job %s is done!", invID)
-	// 	emailService := service.NewEmailService(h.db, h.slog)
-	// 	go func() {
-	// 		err := emailService.SendEmail(2, , "Job Status", msg)
-	// 		if err != nil {
-	// 			h.slog.Error("failed sending email", "err", err.Error())
-	// 		}
-	// 	}()
-	// }
+	if models.JobStatus(newStatus) == models.JOBDONE {
+		msg := fmt.Sprintf("Job %s is done!", invID)
+		emailService := service.NewEmailService(h.db, h.slog)
+		go func() {
+			err := emailService.SendEmail(2, userEmail, "Job Status", msg)
+			if err != nil {
+				h.slog.Error("failed sending email", "err", err.Error())
+			}
+		}()
+	}
 
 	w.Header().Set("HX-Refresh", "true")
 	w.WriteHeader(http.StatusOK)
--- pkg/service/email.go
+++ pkg/service/email.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 	"net/smtp"
 	"os"
-	"sync"
 )
 
 var (
@@ -19,18 +18,16 @@ var (
 type EmailService struct {
 	db   *sql.DB
 	slog *slog.Logger
-	wG   *sync.WaitGroup
 }
 
 func NewEmailService(db *sql.DB, slog *slog.Logger) *EmailService {
 	return &EmailService{
 		db:   db,
 		slog: slog,
-		wG:   &sync.WaitGroup{},
 	}
 }
 
-func (e EmailService) SendEmail(maxWorkers int, recipients []string, subject, msg string) error {
+func (e EmailService) SendEmail(maxWorkers int, recipientsEmail, subject, msg string) error {
 	NotifyHostEmail := os.Getenv("NOTIFY_HOST_EMAIL")
 	NotifyHostPassword := os.Getenv("NOTIFY_HOST_PASSWORD")
 	if NotifyHostEmail == "" {
@@ -43,31 +40,15 @@ func (e EmailService) SendEmail(maxWorkers int, recipients []string, subject, ms
 	smtHost := "smtp.gmail.com"
 	smtPort := "587"
 
-	workerChan := make(chan struct{}, maxWorkers)
-	for _, recipient := range recipients {
-		e.wG.Add(1)
-
-		select {
-		case workerChan <- struct{}{}:
-			go func(recipient string) {
-				defer e.wG.Done()
-				defer func() { <-workerChan }()
-
-				formattedBody := fmt.Sprintf("Hello, \n\n%s!\n\nBest regards,\nNotify", msg)
-				msg := []byte("Subject: " + subject + "\r\n" + "\r\n" + formattedBody + "\r\n")
-				auth := smtp.PlainAuth("", NotifyHostEmail, NotifyHostPassword, smtHost)
-				err := smtp.SendMail(smtHost+":"+smtPort, auth, NotifyHostEmail, []string{recipient}, msg)
-				if err != nil {
-					e.slog.Error("failed to send email", "error", err, "recipient", recipient)
-				} else {
-					e.slog.Info("Successfully sent email")
-				}
-			}(recipient)
-		default:
-			e.slog.Warn("No workers available, retrying in a bit...", "recipient", recipient)
-		}
+	formattedBody := fmt.Sprintf("Hello, \n\n%s!\n\nBest regards,\nNotify", msg)
+	email := []byte("Subject: " + subject + "\r\n" + "\r\n" + formattedBody + "\r\n")
+	auth := smtp.PlainAuth("", NotifyHostEmail, NotifyHostPassword, smtHost)
+	err := smtp.SendMail(smtHost+":"+smtPort, auth, NotifyHostEmail, []string{recipientsEmail}, email)
+	if err != nil {
+		e.slog.Error("failed to send email", "error", err, "recipient", recipientsEmail)
+	} else {
+		e.slog.Info("Successfully sent email")
 	}
-	e.wG.Wait()
 
 	return nil
 }
